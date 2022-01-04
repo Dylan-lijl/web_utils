4f98f76a68ca0076b31f7a37dff00b3991a5fe7d
admin <1754303259@qq.com>
2022-01-04 15:11:24 +0800

    测试项目

---
+++ test/src/main/java/pub/carzy/TestApplication.java
@@ -0,0 +1,16 @@
+package pub.carzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author admin
+ */
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+
+}
